9fe4a1ede8db876d5fc79f5c9728bab617beac8b
Moin Hameed <moinhameed27@gmail.com>
2024-08-18 08:21:57 +0500

    C++ and Java

---
+++ Maths/Ugly Number.cpp
@@ -0,0 +1,43 @@
+// Leetcode : https://leetcode.com/problems/ugly-number/description/
+
+// [C++]----------------------------------------------------------------------------------------------------------------------------
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+                n /= 2;
+            else if (n % 3 == 0)
+                n /= 3;
+            else if (n % 5 == 0)
+                n /= 5;
+            else
+                return false;
+        }
+        return n == 1;
+    }
+};
+
+// [Java]----------------------------------------------------------------------------------------------------------------------------
+class Solution
+{
+public
+    boolean isUgly(int n)
+    {
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+                n /= 2;
+            else if (n % 3 == 0)
+                n /= 3;
+            else if (n % 5 == 0)
+                n /= 5;
+            else
+                return false;
+        }
+        return n == 1;
+    }
+}
